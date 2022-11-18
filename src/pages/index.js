5fbf2a415c5ff4598d15b0f19bad39d34a7bbe44
TahseenSust <tahseenrchowdhury@gmail.com>
2022-11-19 05:37:42 +0600

    added topbar

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Header from "../components/header/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+      {/* ---- TO BEGIN, delete this section and GET CODING!!! ---- */}
+      <Header/>
+      {/* ---- ---- */}
+    </div>
+  );
+}
