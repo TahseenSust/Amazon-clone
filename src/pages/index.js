cc6f535cabc091843fb66daf70df705632d14770
TahseenSust <tahseenrchowdhury@gmail.com>
2022-11-19 23:11:56 +0600

    added products with fake store api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,35 @@
 import Head from "next/head";
-import Header from "../components/header/Header";
+import Header from "../components/Header";
+import Banner from '../components/Banner'
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({products}) {
   return (
-    <div>
+    <div className=" bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
 
       {/* ---- TO BEGIN, delete this section and GET CODING!!! ---- */}
       <Header/>
+
+      <main className=" max-w-screen-2xl mx-auto">
+        <Banner/>
+        <ProductFeed products={products}/>
+      </main>
+
       {/* ---- ---- */}
     </div>
   );
+}
+
+//https://fakestoreapi.com/products
+export async function getServerSideProps(context){
+  const products = await fetch('https://fakestoreapi.com/products')
+                              .then(res=>res.json())
+                              // .then(json=>console.log(json))
+  return {
+    props:{
+      products
+  }}
 }
