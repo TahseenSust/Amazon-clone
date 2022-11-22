7bc97072dad32760236c00ec5830ed1ea9e659be
TahseenSust <tahseenrchowdhury@gmail.com>
2022-11-22 10:45:01 +0600

    added signIn signOut with next Outh

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCN6pi-lxj4p1m6iJP6m87ywQDtJqru0nU",
+    authDomain: "amzn-nextjs-3dba2.firebaseapp.com",
+    projectId: "amzn-nextjs-3dba2",
+    storageBucket: "amzn-nextjs-3dba2.appspot.com",
+    messagingSenderId: "629614690872",
+    appId: "1:629614690872:web:ca5ef7b9b48cc35fe15ccd"
+  };
